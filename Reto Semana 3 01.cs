0407c44bc763a90e7dfb9e7e4460746169d572f2
Gotensfer <77905732+Gotensfer@users.noreply.github.com>
2021-01-30 14:58:22 -0500

    Add files via upload

---
+++ Reto Semana 3 01.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Reto_Semana_3_01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://youtu.be/zrCKnvEaY5w
+
+            //Valor del salario mínimo para el 2021
+            int smmlv = 908526;
+
+            Console.WriteLine("Ingrese su salario mínimo");
+            int salario = int.Parse(Console.ReadLine());
+
+            if (salario < smmlv * 2) //Menos de 2 salarios mínimos
+            {
+                Console.WriteLine("A usted le corresponde la tarifa A");
+            }
+            else if (salario < smmlv * 4) //Entre 2 y menos de 4 salarios mínimos
+            {
+                Console.WriteLine("A usted le corresponde la tarifa B");
+            }
+            else //4 o más salarios mínimos
+            {
+                Console.WriteLine("A usted le corresponde la tarifa C");
+            }
+
+        }
+    }
+}
---
+++ Reto Semana 3 02.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Reto_Semana_3_02
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://youtu.be/djUD5CW20kU
+
+            //Referencia del valor de cuota moderadora 2021
+            //https://www.minsalud.gov.co/sites/rid/Lists/BibliotecaDigital/RIDE/VP/DOA/cuotas-moderadoras-copagos-2021.pdf
+
+
+            int smmlv = 908526;
+
+            Console.WriteLine("Ingrese su salario");
+            int salario = int.Parse(Console.ReadLine());
+
+            if (salario < smmlv * 2)
+            {
+                Console.WriteLine("A usted le corresponde la tarifa A");
+                Console.WriteLine("Con un valor de cuota moderadora de: 3500$");
+            }
+            else if (salario < smmlv * 5)
+            {
+                Console.WriteLine("A usted le corresponde la tarifa B");
+                Console.WriteLine("Con un valor de cuota moderadora de: 14000$");
+            }
+            else
+            {
+                Console.WriteLine("A usted le corresponde la tarifa C");
+                Console.WriteLine("Con un valor de cuota moderadora de: 36800$");
+            }
+        }
+    }
+}
